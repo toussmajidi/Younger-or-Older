49ffefe2be352554f1e585c15a53d2eea9cebdb1
Touss D Majidi <tm73200@srcs.loc>
2020-02-24 10:44:24 -0800

    start

---
+++ algorithm.js
@@ -0,0 +1,6 @@
+function personSelection{
+    var pitbull = new Date(1981, 01, 15);
+    var Teddy = new Date(1858, 10, 27);
+    var Joseph = new Date(1930, 10, 14);
+    var Jojo = new Date(2003, 05, 19);
+}
