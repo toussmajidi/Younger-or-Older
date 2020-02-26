898a9ae20f7b173c48405d364313c488a391e4ad
Touss D Majidi <tm73200@srcs.loc>
2020-02-26 14:42:55 -0800

    added event listners(buttons); need to create birthday function

--- algorithm.js
+++ algorithm.js
@@ -1,6 +1,22 @@
-function personSelection{
-    var pitbull = new Date(1981, 01, 15);
-    var Teddy = new Date(1858, 10, 27);
-    var Joseph = new Date(1930, 10, 14);
-    var Jojo = new Date(2003, 05, 19);
+var btn1 = document.getElementById("Pitbull");
+btn1.addEventListener("click", () => {
+  var pitbull = new Date(1981, 01, 15);
+  birthday(pitbull);
+});
+
+var btn2 = document.getElementById("Teddy");
+btn2.addEventListener("click", () => ); {
+   var Teddy = new Date(1858, 10, 27);
+}
+var btn3 = document.getElementById("Joseph");
+btn3.addEventListener("click", () => ); {
+   var Joseph = new Date(1930, 10, 14);
+}
+var btn4 = document.getElementById("Jojo");
+btn4.addEventListener("click", () => ); {
+   var Jojo = new Date(2003, 05, 19);
+}
+
+function birthday(date){
+    console.log(date);
 }
