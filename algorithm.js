e097c4d08aa5271fbfa77c9e7f44b14d57efe2c4
toussmajidi <toussmajidi@yahoo.com>
2020-03-01 18:17:26 -0800

    Finished most of algorithm. Still needs tweaking

--- algorithm.js
+++ algorithm.js
@@ -20,7 +20,78 @@ btn4.addEventListener("click", () => {
     birthday(Jojo);
 });
 
+function validEntry() {
+  var val1 = document.getElementById("m").value;
+  var val2 = document.getElementById("d").value;
+  var val3 = document.getElementById("y").value;
+  var validEntries;
+  var msg = "";
+  if(val1 == "" || val2 == "" || val3 == "") {
+    msg = "You need to enter something first!";
+    validEntries = false;
+  } else if (isNaN(val1) || isNaN(val2) || isNaN(val3)) {
+    msg = "That's not a number!";
+    validEntries = false;
+  } else if (val1 % 1 !== 0 || val2 % 1 !== 0 || val3 % 1 !==0) {
+    msg = "Enter an integer!";
+    validEntries = false;
+  } else if(val1 < 0 || val2 < 0 || val3 < 0){
+    msg = "Positive entries only!!"
+    validEntries = false;
+  }else {
+    validEntries = true;
+  }
+  document.getElementById("bdaymsg").innerHTML = msg;
+  if(!validEntries){
+    document.getElementById("m").style.borderColor = "red";
+    document.getElementById("d").style.borderColor = "red";
+    document.getElementById("y").style.borderColor = "red";
+    return false;
+  } else {
+    document.getElementById("m").style.borderColor = "lightGrey";
+    document.getElementById("d").style.borderColor = "lightGrey";
+    document.getElementById("y").style.borderColor = "lightGrey";
+    return true;
+  }
+}
+
+function validDate(){
+  var month = document.getElementById("m").value;
+  var day = document.getElementById("d").value;
+
+  if(month < 1 || month > 12){
+    document.getElementById("bdaymsg").innerHTML = "Invalid month";
+    return false;
+  }
+
+  if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+    if(day < 1 || day > 31){
+      document.getElementById("bdaymsg").innerHTML = "Invalid day";
+      return false;
+    }
+  }
+
+  if(month == 2){
+    if(day < 1 || day > 28){
+      document.getElementById("bdaymsg").innerHTML = "Invalid day";
+      return false;
+    }
+  }
+
+  if(month == 4 || month == 6 || month == 9 || month == 11){
+    if(day < 1 || day > 30){
+      document.getElementById("bdaymsg").innerHTML = "Invalid day";
+      return false;
+    }
+  }
+
+  console.log("valid date");
+  return true;
+
+}
+
 function birthday(date) {
+  if(validDate() && validEntry()){
     var msg = "";
     var m = parseInt(document.getElementById("m").value);
     var d = parseInt(document.getElementById("d").value);
@@ -29,18 +100,19 @@ function birthday(date) {
     var milli = date.getTime() - bday.getTime();
 
     var days =  Math.ceil(milli/86400000);
-    var years Math.ceil(milli/31536000000);
+    //var years Math.ceil(milli/31536000000);
     if(days < 0){
-        msg = "You are " + -days + " days older.";
+        msg = "You are " + -days + " days younger.";
     } else if(days == 0){
         msg = "Wow! You're twins!";
     } else{
-        msg = "You are " + days + " days younger";
+        msg = "You are " + days + " days older";
     }
-    return msg;
+  console.log(msg);
 
-    var div = document.getElementById("input");
+    var div = document.getElementById("bdaymsg");
     var sent = document.createElement("p");
     div.appendChild(sent);
     sent.textContent = msg;
+}
 }
